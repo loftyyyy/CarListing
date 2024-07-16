f0b1fd0c32b959e32fe5fc47669646d3c2a3ff7d
loftyyyy <lofty08.business@gmail.com>
2024-07-17 00:00:06 +0800

    damn

--- src/main/java/org/example/carlisting/Cars/Car.java
+++ src/main/java/org/example/carlisting/Cars/Car.java
@@ -1,6 +1,98 @@
 package org.example.carlisting.Cars;
 
 
-public class Car {
+import java.awt.*;
 
+public class Car {
+    private Long id;
+    private int carModelYear;
+    private int price;
+    private String registrationNumber;
+    private String brand;
+    private String model;
+    private String description;
+    private Color color;
+
+    public Car(){
+
+    }
+    public Car(int carModelYear, int price, String registrationNumber, String brand, String model, String description, Color color) {
+        this.carModelYear = carModelYear;
+        this.price = price;
+        this.registrationNumber = registrationNumber;
+        this.brand = brand;
+        this.model = model;
+        this.description = description;
+        this.color = color;
+    }
+
+    public int getCarModelYear() {
+        return carModelYear;
+    }
+
+    public void setCarModelYear(int carModelYear) {
+        this.carModelYear = carModelYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", carModelYear=" + carModelYear +
+                ", price=" + price +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", description='" + description + '\'' +
+                ", color=" + color +
+                '}';
+    }
 }
