b40f13cf901ba5735181ab06a7c6af343aece634
loftyyyy <lofty08.business@gmail.com>
2024-07-16 23:15:45 +0800

    initialCommit

---
+++ src/main/java/org/example/carlisting/CarListingApplication.java
@@ -0,0 +1,13 @@
+package org.example.carlisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarListingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarListingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/carlisting/CarListingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.carlisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarListingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
