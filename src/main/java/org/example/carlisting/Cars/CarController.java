ef58f485127922678afd7831efe8f3c8a088df91
loftyyyy <lofty08.business@gmail.com>
2024-07-17 20:44:12 +0800

    el

--- src/main/java/org/example/carlisting/Cars/CarController.java
+++ src/main/java/org/example/carlisting/Cars/CarController.java
@@ -1,10 +1,16 @@
 package org.example.carlisting.Cars;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CarController {
 
 
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
 
 }
