122da43a07495a3392f1ef39bed63478447c4921
loftyyyy <lofty08.business@gmail.com>
2024-07-18 19:26:24 +0800

    h

--- src/main/java/org/example/carlisting/Cars/CarController.java
+++ src/main/java/org/example/carlisting/Cars/CarController.java
@@ -1,5 +1,6 @@
 package org.example.carlisting.Cars;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +10,29 @@ public class CarController {
 
 
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+
         return "index";
+    }
+
+    @GetMapping("/browse")
+    public String browse(Model model){
+        return "browse";
+    }
+
+    @GetMapping("/sell")
+    public String sell(Model model){
+        return "sell";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model){
+        return "contact";
     }
 
     //TODO: Make the header responsive, put a marker to know which page th euser is
